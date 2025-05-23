a5aff92289f8a2a4c471f03b5bdb5bdb89b89415
Laksh <laksh.maheshwari10@gmail.com>
2025-05-23 21:03:37 +0530

    .

--- src/services/api/endpoints.ts
+++ src/services/api/endpoints.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://127.0.0.1:5000';
+export const BASE_URL = 'http://192.168.31.39:5000';
 // export const BASE_URL = import.meta.env.VITE_API_BASE_URL || console.error('API URI is not set');
 
 export const createEndpoint = (path: string) => `${BASE_URL}${path}`;
